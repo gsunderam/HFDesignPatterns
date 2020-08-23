3d8afd5c29b977584acc60450f66010d65ae3449
gsunderam <gsunder23@rediffmail.com>
2020-08-23 16:45:44 -0700

    using atomic integer in lieu of synchronized

--- src/tests/FizzBuzzThread.java
+++ src/tests/FizzBuzzThread.java
@@ -2,16 +2,16 @@ package tests;
 
 import log.Logger;
 
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
 
 /**
  * Classic fizzBuzz interview question done using 4 threads. See {@link FizzBuzzTest}
  */
 public class FizzBuzzThread extends Thread {
-    Function<Integer, String> function;
-    int current = 1;
-    Object lock = new Object();
-    int max;
+    private final Function<Integer, String> function;
+    private final AtomicInteger current = new AtomicInteger(1);
+    private int max;
 
     public FizzBuzzThread(Function<Integer, String> function, int max) {
         this.function = function;
@@ -20,12 +20,10 @@ public class FizzBuzzThread extends Thread {
 
     @Override
     public void run() {
-        synchronized (lock) {
-            while (current <= max) {
-                String result = function.apply(current);
-                if (result != null) Logger.stdout("[" + current + "] " + result);
-                current++;
-            }
+        while (current.get() <= max) {
+            String result = function.apply(current.intValue());
+            if (result != null) Logger.stdout("[" + current.intValue() + "] " + result);
+            current.incrementAndGet();
         }
     }
 }
